0b035a8c7213e7d7c651ead274fb0d1c100615ef
Harald Berge <hb@xhbml.com>
2016-01-30 11:11:30 +0100

    Added latest_articles method

--- related/related.py
+++ related/related.py
@@ -30,6 +30,31 @@ def related_via_tags(cls, articles, categories=[], exclude_categories=[],limit=-
         related = related[:limit]
     return related
 
+def latest_articles(cls, articles, categories=[], exclude_categories=[], limit=-1, age=300):
+    return []
+    def exclude_old(articles, age):
+        if articles:
+            print time.mktime(articles[0].date)
+            if age:
+                print age
+            
+    def filter_this(related, categories, exclude_categories):
+        if related:
+            related = filter(lambda article: article.source_path != cls.source_path, related) # Remove self
+            if categories:
+                related = filter(lambda article: str(article.category) in categories, related)
+            if exclude_categories:
+                related = filter(lambda article: str(article.category) not in exclude_categories, related)
+        return related
+    def getDate(art):
+        return art.date
+    articles = exclude_old(articles, age)
+    articles = sorted(articles, key=getDate, reverse=True)
+    selection = filter_this(articles, categories, exclude_categories)
+    if limit:
+        selection = selection[:limit]
+    return selection
+
 def add_related_via_tags_method(sender):
     contents.Article.related_via_tags = related_via_tags
     contents.Draft.related_via_tags = related_via_tags
